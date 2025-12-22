55d7e1020feb324183fd9fdbb05893181b0d6171
tutac <tutachasan@gmail.com>
2025-12-22 15:25:03 -0500

    added video to homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,11 +3,13 @@ import FeatureGrid from '@/components/home/FeatureGrid'
 import Pricing from '@/components/Pricing'
 import StepsSection from '@/components/home/StepsSection'
 import TestimonialsSection from '@/components/home/TestimonialsSection'
+import VideoTutorialSection from '@/components/home/VideoTutorialSection'
 
 export default function Page() {
   return (
     <div className="flex flex-col">
       <HeroSection />
+      <VideoTutorialSection />
       <FeatureGrid />
       <Pricing />
       <StepsSection />
